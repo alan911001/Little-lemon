ec3caf44595b6ad588c456f73c0791a3780d1fc8
Alan Conteras <a.contreras@ux.edu.mx>
2026-04-25 09:16:52 -0600

    Estilizando texto, elementos NAV e imagenes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,27 @@
 import { ItemsNav } from "./ItemsNav";
-import { Nav } from "./Nav";
 import image_footer from "../assets/restaurant.jpg";
 
 export const Footer = () => {
   return (
     <footer className="container">
-      <img src={image_footer} className="img_footer" />
+      <img src={image_footer} alt="restaurant" className="img_footer" />
       <div>
         <h3>Doormat Navigation</h3>
-        <ItemsNav />
+        <ItemsNav classText="nav-items-footer" />
       </div>
       <div>
         <h3>Contact</h3>
-        <ul>
-          <li>Address</li>
-          <li> Phone </li>
-          <li>number</li>
-          <li>email</li>
-        </ul>
+        <p>Address</p>
+        <p> Phone </p>
+        <p>number</p>
+        <p>email</p>
       </div>
       <div>
         <h3>Social media links</h3>
-        <ul>
-          <li>Address</li>
-          <li> Phone </li>
-          <li>number</li>
-          <li>email</li>
-        </ul>
+        <p>Address</p>
+        <p> Phone </p>
+        <p>number</p>
+        <p>email</p>
       </div>
     </footer>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,17 @@
+import image from "../assets/restauranfood.jpg";
+
 export const Main = () => {
   return (
     <main className="container">
-      <h1>Main</h1>
+      <div>
+        <h1>Little lemon</h1>
+        <h3>Chicago</h3>
+        <p>
+          We are a family owned Mediterranean restaurant, focused on traditional
+          recipies served whit a modern twist.
+        </p>
+      </div>
+      <img src={image} alt="" />
     </main>
   );
 };
